16812d91f0d4c05e00bfaa6f2ebd0ff5ef298c45
RudenkoArts <rudenkopaint@gmail.com>
2018-02-10 23:24:39 +0200

    added modules

---
+++ sparkler/modules/AreaEdgeSpawnModule.hx
@@ -0,0 +1,121 @@
+package sparkler.modules;
+
+import sparkler.core.Particle;
+import sparkler.core.ParticleData;
+import sparkler.core.ParticleModule;
+import sparkler.core.Components;
+
+import luxe.Vector;
+
+using sparkler.utils.VectorTools;
+
+
+class AreaEdgeSpawnModule  extends ParticleModule {
+
+
+	public var size(default, null):Vector;
+	public var size_max(default, null):Vector;
+
+	var rnd_point:Vector;
+
+
+	public function new(_options:AreaEdgeSpawnModuleModuleOptions) {
+
+		super(_options);
+
+		size = _options.size != null ? _options.size : new Vector(64, 64);
+		size_max = _options.size_max != null ? _options.size_max : new Vector(128, 128);
+
+		rnd_point = new Vector();
+
+		priority = -999;
+		
+	}
+
+	override function onspawn(p:Particle) {
+
+		var pd:ParticleData = emitter.add_to_bacher(p);
+
+		var sx:Float = emitter.random_float(size.x, size_max.x);
+		var sy:Float = emitter.random_float(size.y, size_max.y);
+
+		random_point_on_rect_edge(sx, sy);
+		rnd_point.x -= sx * 0.5;
+		rnd_point.y -= sy * 0.5;
+		pd.x = emitter.system.position.x + emitter.position.x + rnd_point.x;
+		pd.y = emitter.system.position.y + emitter.position.y + rnd_point.y;
+
+	}
+
+	override function onunspawn(p:Particle) {
+
+		emitter.remove_from_bacher(p);
+
+	}
+
+	inline function random_point_on_rect_edge(width:Float, height:Float) : Vector {
+
+		var rnd_edge_len = emitter.random() * (width * 2 + height * 2);
+
+		if (rnd_edge_len < height){
+
+			rnd_point.x = 0;
+			rnd_point.y = height - rnd_edge_len;
+			
+		} else if (rnd_edge_len < (height + width)){
+
+			rnd_point.x = rnd_edge_len - height;
+			rnd_point.y = 0;
+
+		} else if (rnd_edge_len < (height * 2 + width)){
+
+			rnd_point.x = width;
+			rnd_point.y = rnd_edge_len - (width + height);
+
+		} else {
+
+			rnd_point.x = width - (rnd_edge_len - (height * 2 + width));
+			rnd_point.y = height;
+
+		}
+
+		return rnd_point;
+
+	}
+
+
+// import/export
+
+	override function from_json(d:Dynamic) {
+
+		super.from_json(d);
+
+		size.from_json(d.size);
+		size_max.from_json(d.size_max);
+
+		return this;
+	    
+	}
+
+	override function to_json():Dynamic {
+
+		var d = super.to_json();
+
+		d.size = size.to_json();
+		d.size_max = size_max.to_json();
+
+		return d;
+	    
+	}
+
+
+}
+
+typedef AreaEdgeSpawnModuleModuleOptions = {
+
+	>ParticleModuleOptions,
+
+	@:optional var size:Vector;
+	@:optional var size_max:Vector;
+
+}
---
+++ sparkler/modules/DirectionFromAngleModule.hx
@@ -0,0 +1,138 @@
+package sparkler.modules;
+
+import sparkler.core.Particle;
+import sparkler.core.ParticleModule;
+import sparkler.core.ParticleData;
+import sparkler.core.Components;
+import sparkler.components.Velocity;
+import sparkler.modules.helpers.VelocityUpdateModule;
+
+
+
+class DirectionFromAngleModule extends ParticleModule {
+
+
+	public var direction_variance:Float;
+	public var speed:Float;
+	public var speed_variance:Float;
+
+	var vel_comps:Components<Velocity>;
+	var particles_data:Array<ParticleData>;
+
+
+	public function new(_options:DirectionFromAngleModuleOptions) {
+
+		super(_options);
+
+		direction_variance = _options.direction_variance != null ? _options.direction_variance : 0;
+		speed = _options.speed != null ? _options.speed : 60;
+		speed_variance = _options.speed_variance != null ? _options.speed_variance : 0;
+
+	}
+
+	override function init() {
+
+		if(emitter.get_module(VelocityUpdateModule) == null) {
+			emitter.add_module(new VelocityUpdateModule());
+		}
+
+		vel_comps = emitter.components.get(Velocity);
+
+		particles_data = emitter.particles_data;
+
+	}
+
+	override function ondisabled() {
+
+		for (p in particles) {
+			vel_comps.get(p).set_xy(0,0);
+		}
+		
+	}
+	
+	override function onremoved() {
+
+		emitter.remove_module(VelocityUpdateModule);
+		vel_comps = null;
+		particles_data = null;
+		
+	}
+
+	override function onspawn(p:Particle) {
+
+		var angle:Float = 0;
+		var spd:Float = speed;
+
+		var pd:ParticleData = particles_data[p.id];
+		angle = angle_between_points(
+				emitter.system.position.x + emitter.position.x, 
+				emitter.system.position.y + emitter.position.y, 
+				pd.x,
+				pd.y
+			);
+
+		if(direction_variance != 0) {
+			angle += (direction_variance * emitter.random_1_to_1()) * 0.017453292519943295;
+		}
+
+		if(speed_variance != 0) {
+			spd += speed_variance * emitter.random_1_to_1();
+		}
+
+		var v:Velocity = vel_comps.get(p);
+		v.x = spd * Math.cos(angle);
+		v.y = spd * Math.sin(angle);
+
+	}
+
+	inline function angle_between_points(ax:Float, ay:Float, bx:Float, by:Float):Float {
+
+		var dy:Float = by - ay;
+		var dx:Float = bx - ax;
+
+		return Math.atan2(dy, dx);
+
+	}
+
+
+// import/export
+
+	override function from_json(d:Dynamic) {
+
+		super.from_json(d);
+
+		direction_variance = d.direction_variance;
+		speed = d.speed;
+		speed_variance = d.speed_variance;
+
+		return this;
+	    
+	}
+
+	override function to_json():Dynamic {
+
+		var d = super.to_json();
+
+		d.direction_variance = direction_variance;
+		d.speed = speed;
+		d.speed_variance = speed_variance;
+
+		return d;
+	    
+	}
+
+
+}
+
+
+typedef DirectionFromAngleModuleOptions = {
+
+	>ParticleModuleOptions,
+	
+	@:optional var direction_variance : Float;
+	@:optional var speed : Float;
+	@:optional var speed_variance : Float;
+
+}
+
+
